e9e4c056b3f1a558bf92a72ffdc90f722fab0463
jtomala <jtomala@students.42wolfsburg.de>
2022-06-22 07:40:55 +0200

    switched two mains; now it's working

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -1,11 +1,15 @@
 #include "Harl.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
-	Harl object;
-
-	object.complain("DEBUG");
-	object.complain("INFO");
-	object.complain("WARNING");
-	object.complain("ERROR");
+	if (argc != 2) {return 1;}
+	Harl Steve;
+	std::string level = argv[1];
+	if (!level.compare("DEBUG") || !level.compare("INFO") || !level.compare("WARNING") || !level.compare("ERROR")){
+		Steve.complain(level);
+	}
+	else{
+		std::cout << "[  Probably complaining about insignificant problems ]" << std::endl;
+	}
+	return 0;
 }
