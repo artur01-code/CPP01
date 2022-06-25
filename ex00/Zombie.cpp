a2551624f238d96842f7810456684ebb15d2165b
jtomala <jtomala@students.42wolfsburg.de>
2022-06-25 13:08:34 +0200

    public functions

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -20,13 +20,3 @@ void Zombie::announce(void)
 	std::cout << "\033[32m" << get_name() << " : \033[0m BraiiiiiiinnnzzzZ..." << std::endl; 
 };
 
-Zombie* Zombie::newZombie(std::string name)
-{
-	return (new Zombie(name));
-};
-
-void Zombie::randomChump(std::string name)
-{
-	Zombie random(name);
-	random.announce();
-}
